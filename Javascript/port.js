fe27b5a253ceeea69317b2994ae95860cd4d7a0e
Someshg007 <ssgggundayya2002@gmail.com>
2024-03-02 01:52:42 +0530

    new

---
+++ Javascript/port.js
@@ -0,0 +1,6 @@
+let content_box=document.getElementsByClassName("content-box")[0];
+content_box.addEventListener('mouseenter',()=>
+{
+    content_box.style.color="black";
+    console.log("Running Succesfully")
+})
